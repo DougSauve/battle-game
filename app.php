96574f56528b1a64d76564694ff681a52bb3d165
Doug Sauve <35982843+DougSauve@users.noreply.github.com>
2018-02-19 15:53:10 -0600

    Add files via upload

--- app.php
+++ app.php
@@ -1,11 +1,35 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <script src = "objects.js"></script>
+    <script src = "../jQuery.js"></script>
+    <script src = "./objects.js"></script>
   </head>
   <body>
     <script>
 
+    initializeGame(15,15);
+
+    //initialize everything
+    function initializeGame(hexesHigh, hexesWide) {
+
+//these all need to be positioned with css
+    const gs = new GameSpace();
+    const m = new Menu();
+    const a = new Area(gs.getSmallestDimension(), hexesHigh, hexesWide);
+
+    //create an array to hold all of the hexes
+    var hexes = [];
+
+    //create the hexes, and insert them into (dynamically created) nested arrays within 'hexes'.
+    for (let z = 0; z < hexesHigh; z++){
+      for (let y = 0; y < hexesWide; y++){
+        hexes[z][y] = new Hex(z * a.getHexLength(), y * a.getHexLength(), a.getHexLength()); // for these arguments, get the location and size from a.)
+      }
+    }
+
+
+
+  }
     </script>
   </body>
 </html>
--- objects.js
+++ objects.js
@@ -1,36 +1,101 @@
-function CreateGameSpace () {
-  //set the layout for the game.
+function GameSpace () {
+  //set the layout for the game:
 
-  //if the screen's smallest dimension is smaller than 400px(?), the layout has to be contracted.
+  //get the height and width of the viewport
+  const gameHeight = $(window).height();
+  const gameWidth = $(window).width();
 
+  //set smallestDimension and biggestDimension to portrait view
+  var smallestDimension = gameWidth;
+  var biggestDimension = gameHeight;
+
+  //switch to landscape if necessarry
+  if (gameHeight < gameWidth) {
+    smallestDimension = gameHeight;
+    biggestDimension = gameWidth;
+  }
+
+  //set layout for a small screen. If the screen's smallest dimension is smaller than 400px(?), expanded layout is false.
+  var expandedLayout = false;
+
+  //if it's between 400px and 600px and the bigger dimension is at least 133% of the smaller one, it's expanded.
+  if ((smallestDimension >= 400)&&(smallestDimension < 600)) {
+    if (biggestDimension * 3 >= smallestDimension * 4) {
+      expandedLayout = true;
+    }
+  }
   //If it's bigger than 600px(?) it will be expanded.
+  else if (smallestDimension >= 600) {
+    expandedLayout = true;
+  }
 
-  //if it's in between, if the bigger dimension is at least 133% of the smaller one, it's expanded; otherwise it's contracted.
+  //create methods to return all of these as needed
+  function getSmallestDimension() {
+    return smallestDimension;
+  }
+  function getBiggestDimension() {
+    return biggestDimension;
+  }
+  function isLayoutExpanded() {
+    return expandedLayout;
+  }
+  function getGameHeight() {
+    return gameHeight;
+  }
+  function getGameWidth () {
+    return gameWidth;
+  }
 
-  //get the smallest dimension
+  //create its own div
+  /*  let gs = document.createElement("GameSpace");
+  gs.setAttribute("id", "GameSpace");
+  gs.style = `width: ${gameWidth}; height: ${gameHeight}; background-color: red`;
+  document.body.appendChild(gs);
+  */
+  $('body').append(`<div id = "GameSpace" style = "width: ${gameWidth}; height: ${gameHeight}; position: absolute; background-color: red"></div>`);
+}
 
-  //set smallestDimension
+function Area (smallestDimension) {
 
-  //set biggestDimension
+  //create a hexesHigh x hexesWide area based on the smallest dimension - it will be 90% of the smallest dimension and slightly offset, leaving room for little information panels along the top and side.
 
-  //Set layout according to the screen size and ratio.
+  //set side length of board
+  const boardLength = smallestDimension * 0.9;
+
+  //set side length of hexes
+  const hexLength = Math.floor(boardLength / 15);
+
+  function getBoardLength() {
+    return boardLength;
+  }
+  function getHexLength() {
+    return hexLength;
+  }
+  //create the Area div
+  $('body').append(`<div id = "Area" style = "width: ${getBoardLength}; height: ${getBoardLength}; background-color: blue; position: absolute; left: 50px; right: 50px"></div>`);
+
 
 }
 
-function CreateArea (smallest dimension) {
-  //get the smallest dimension from the GameSpace
+function Hex (verticalCoordinate, horizontalCoordinate, sideLength) {
 
-  //create a 15x15 area based on the smallest dimension - it will be 90% of the smallest dimension, leaving room for little information panels along the top and side.
+  //create the Hex div
+  $('#Area').append(`<div style = "display: absolute; top: ${verticalCoordinate}; left: ${horizontalCoordinate}; height: ${sideLength}; width: ${sideLength}; background-color: yellow"></div>`);
 
-  //define the positions and size for the hexes and store these in nested arrays.
 
+  var terrain;
+
+  this.setTerrain("meadow");
+
+  function setTerrain(terr) {
+    terrain = terr;
+  }
+  function getTerrain() {
+    return terrain;
+  }
 }
 
-function CreateHex (vertical coordinate, horizontal coordinate, height, width) {
-  //for each Hex space that CreateArea defines, create a Hex there. A Hex needs a terrain type, somewhat randomly assigned.
-}
-
-function CreateDash (smallest dimension, biggest dimension, layout) {
+function Menu (smallest dimension, biggest dimension, layout) {
 //get the layout and smallest dimension from the GameSpace
 
 //if the layout is expanded, make the dash fit beside/below it
