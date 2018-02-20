3aaca8883be62b7274621dfa1eab0d5722e7ee8d
Doug Sauve <35982843+DougSauve@users.noreply.github.com>
2018-02-20 14:42:28 -0600

    Movement good
    Movement with arrow keys is working. Next steps are to get Enemy and Pawn classes working and Teammate and E inheriting from P.
    
    To recreate this, put the pictures into a subfolder called 'Photos'.

--- app.php
+++ app.php
@@ -13,27 +13,50 @@
       //initialize everything
       function initializeGame(hexesWide, hexesHigh) {
 
+        //set the page margin and padding to 0
+        document.body.style.margin = 0;
+        document.body.style.padding = 0;
+        document.body.style.boxSizing = "border-box";
+
         //these all need to be positioned with css
         const gs = new GameSpace();
         const m = new Menu();
         const a = new Area(gs.getSmallestDimension(), gs.getGameHeight(), gs.getGameWidth(), gs.isLandscape(), hexesHigh, hexesWide);
 
         //create an array to hold all of the hexes
-        var hexes = [];
+        var hex = [];
 
         //create the hexes, and insert them into (dynamically created) nested arrays within 'hexes'.
         for (let z = 0; z < hexesHigh; z++){
 
-          hexes[z] = [];
+          hex[z] = [];
 
           for (let y = 0; y < hexesWide; y++){
-            hexes[z][y] = new Hex(z, y, z * a.getHexLength(), y * a.getHexLength(), a.getHexLength()); // for these arguments, get the location and size from a.)
+            hex[z][y] = new Hex(y, z, z * a.getHexLength(), y * a.getHexLength(), a.getHexLength()); // for these arguments, get the location and size from a.)
           }
         }
-        let grab = document.getElementById("Hex0by7");
-        grab.style.backgroundColor = "";
-        grab.style.backgroundImage = "url('Sky.jpg')";
-        grab.style.backgroundSize = "cover";
+
+        const t1 = new Teammate();
+
+        //set Teammate into its starting position
+        t1.setStartPosition(hex[0][5]);
+
+        //listener for arrow keys.
+        document.onkeydown = getKeyPressed;
+        function getKeyPressed(e) {
+          e = e || window.event;
+
+          if (e.keyCode == '37') t1.move(hex, "left");
+          if (e.keyCode == '38') t1.move(hex, "up");
+          if (e.keyCode == '39') t1.move(hex, "right");
+          if (e.keyCode == '40') t1.move(hex, "down");
+        }
+
+
+
+
+
+
       }
     </script>
   </body>
--- objects.js
+++ objects.js
@@ -1,3 +1,4 @@
+//provides information and dimensions to other classes and contains the whole game.
 function GameSpace () {
   //set the layout for the game:
 
@@ -34,37 +35,39 @@ function GameSpace () {
   $('body').append(`<div id = "GameSpace" style = "width: ${gameWidth}px; height: ${gameHeight}px; position: absolute; background-color: grey"></div>`);
 
   //methods to return all of these as needed
-    this.getSmallestDimension = function() {
-      return smallestDimension;
-    };
-    this.getBiggestDimension = function() {
-      return biggestDimension;
-    };
-    this.isLayoutExpanded = function() {
-      return expandedLayout;
-    }
-    this.getGameHeight = function() {
-      return gameHeight;
-    }
-    this.getGameWidth = function () {
-      return gameWidth;
-    }
-    this.isLandscape = function () {
-      return isLandscape;
-    }
+  this.getSmallestDimension = function() {
+    return smallestDimension;
+  };
+  this.getBiggestDimension = function() {
+    return biggestDimension;
+  };
+  this.isLayoutExpanded = function() {
+    return expandedLayout;
+  }
+  this.getGameHeight = function() {
+    return gameHeight;
+  }
+  this.getGameWidth = function () {
+    return gameWidth;
+  }
+  this.isLandscape = function () {
+    return isLandscape;
+  }
 
 }
-//create methods to return all of these as needed
 
+//this is the play area where the actual game happens.
 function Area (smallestDimension, gameHeight, gameWidth, isLandscape, hexesHigh, hexesWide) {
 
   //create a hexesHigh x hexesWide area based on the smallest dimension - it will be 90% of the smallest dimension and slightly offset, leaving room for little information panels along the top and side.
-
   var hexLength, boardWidth, boardHeight;
 
+
     if (hexesHigh > hexesWide) {
+      //the longest dimension of Area will always be 90% of the smallest dimension.
       boardHeight = smallestDimension * 0.9;
       hexLength = boardHeight / hexesHigh;
+      //determine the other dimension relative to the number of hexes going that way
       boardWidth = hexLength * hexesWide;
     } else {
       boardWidth = smallestDimension * 0.9;
@@ -76,8 +79,11 @@ function Area (smallestDimension, gameHeight, gameWidth, isLandscape, hexesHigh,
   //create the Area div
   $('body').append(`<div id = "Area" style = "width: ${boardWidth}px; height: ${boardHeight}px; background-color: #553344; position: absolute; left: 0; right: 0; margin: ${gameHeight/20}px auto;"></div>`);
 
-  this.getBoardLength = function() {
-    return boardLength;
+  this.getBoardWidth = function() {
+    return boardWidth;
+  }
+  this.getBoardHeight = function() {
+    return boardHeight;
   }
   this.getHexLength = function() {
     return hexLength;
@@ -85,52 +91,147 @@ function Area (smallestDimension, gameHeight, gameWidth, isLandscape, hexesHigh,
 
 }
 
-function Hex (verticalPosition, horizontalPosition,  verticalCoordinate, horizontalCoordinate, sideLength) {
+//this is one hex on the board (there will be many instances of this.)
+function Hex (verticalPosition, horizontalPosition,  horizontalCoordinate, verticalCoordinate, sideLength) {
 
   var terrain = getRandomTerrain();
+  var image = "";
 
-  var backgroundColor = setBackgroundColor(terrain);
+  const divId = `Hex${horizontalPosition}x${verticalPosition}`;
 
   //create the Hex div
-  $('#Area').append(`<div id = "Hex${horizontalPosition}by${verticalPosition}"; style = "position: absolute; top: ${verticalCoordinate}px; left: ${horizontalCoordinate}px; height: ${sideLength}px; width: ${sideLength}px; background-color: ${backgroundColor}; border-radius: 8px 3px 8px 3px"></div>`);
+  $('#Area').append(`<div id = "${divId}"; style = "position: absolute; top: ${verticalCoordinate}px; left: ${horizontalCoordinate}px; height: ${sideLength}px; width: ${sideLength}px; background: url('./Photos/${terrain}'); background-size: cover; border: solid black .2vw; border-radius: 8px 3px 8px 3px"></div>`);
 
+
+  const div = document.getElementById(divId);
+
+  this.getDivId = function() {
+    return divId;
+  }
   this.setTerrain = function(terr) {
     terrain = terr;
   }
   this.getTerrain = function() {
     return terrain;
   }
+
+  //sets image to the given argument and updates the hex's corresponding div with the argument. If img is undefined, sets the image to "" and removes the image from the div.
+  this.setImage = function(img) {
+    if (img == undefined)
+    {
+        image = "";
+        div.innerHTML = "";
+    } else {
+      image = img;
+
+      div.innerHTML = '<img src = "' + image + '" height = "' + sideLength + '" width = "' + sideLength + '">';
+    }
+  }
+  this.getImage = function() {
+    return image;
+  }
+
   //generates a random terrain
   function getRandomTerrain() {
     let num = getRand(1,5);
 
-    if (num === 1) return "forest";
-    else if (num === 2) return "meadow";
-    else if (num === 3) return "swamp";
-    else if (num === 4) return "water";
-    else if (num === 5) return "mountain";
-  }
-  //assign the appropriate background color to the terrain
-  function setBackgroundColor(terr) {
-    if (terr === "forest") {
-      return "#33dd33";
-    } else if (terr === "meadow") {
-      return "#99dd55";
-    } else if (terr === "swamp") {
-      return "#335533";
-    } else if (terr === "water") {
-      return "#1144ee";
-    } else if (terr === "mountain") {
-      return "#553322";
-    }
+    if (num === 1) return "Forest";
+    else if (num === 2) return "Plains";
+    else if (num === 3) return "Swamp";
+    else if (num === 4) return "Water";
+    else if (num === 5) return "Mountain";
   }
 }
 
+//this is the information area, if needed.
 function Menu (smallestDimension, biggestimension, layout) {
-//get the layout and smallest dimension from the GameSpace
 
-//if the layout is expanded, make the dash fit beside/below it
+  //get the layout and smallest dimension from the GameSpace
 
-//if the layout is contracted, make the dash fit over it.
+  //if the layout is expanded, make the dash fit beside/below it
 
+  //if the layout is contracted, make the dash fit over it.
+
+}
+
+function Pawn() {
+
+  this.Move = function() {
+    //make this one's image disappear, and make the corresponding one's image reappear.
+  }
+  //what do they all need?
+  //race (from DB)
+  //current position
+  //image
+  //stats (race), attack, defence, HP, speed, etc.
+
+  //Methods: move, attack
+}
+function Teammate() {
+
+  var currentHex;
+  var image = "./Photos/Horse.png";
+
+
+  this.setImage = function(img) {
+    image = img;
+  }
+  this.getImage = function() {
+    return image;
+  }
+
+  //sets the starting position by calling the appropriate hex and giving it the Teammate's picture
+  this.setStartPosition = function(hex) {
+
+    currentHex = hex;
+
+    currentHex.setImage(image);
+  }
+
+  //moves the Teammate by calling the current hex and removing its picture, then calling the new hex and giving it the Teammate's picture.
+  this.move = function(hexArray, direction) {
+    //set the image in the new hex
+    var temp = currentHex.getDivId().split("x");
+
+    if (direction === "up") {
+      temp[2]--;
+    }
+    else if (direction === "down") {
+      temp[2]++;
+    }
+    else if (direction === "left") {
+      temp[1]--;
+    }
+    else if (direction === "right") {
+      temp[1]++;
+    }
+
+    //if the user is trying to go somewhere that doesn't exist, don't move anything and fire an alert.
+    if (hexArray[temp[1]]) {
+      if (hexArray[temp[1]][temp[2]]) {
+
+        toHex = hexArray[temp[1]][temp[2]];
+        //send no parameter to remove the image
+        currentHex.setImage();
+        toHex.setImage(image);
+
+        currentHex = toHex;
+      }else{
+        alert("you can't go there.");
+      }
+    }else{
+      alert("you can't go there.");
+    }
+
+  }
+
+
+  //the good guys
+  //race chosen from good guys
+  //good guy marker
+}
+function Enemy() {
+  //the bad guys
+  //race chosen from bad guys
+  //bad guy marker
 }
